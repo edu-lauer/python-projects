331b6e200c1b93043943d98971e09bda84ec7e7f
edu-lauer <lauer.edu@gmail.com>
2022-05-19 09:08:39 -0700

    aplicação web django integrado com banco de dados SQLite

---
+++ Django/teste.py
@@ -0,0 +1,19 @@
+receitas = {
+    1: 'Lasanha',
+    2: 'Sopa de Legumes',
+    3: 'Sorvete',
+    4: 'Bolo de Cenoura'
+}
+
+dados = {
+    'nome_das_receitas': receitas
+}
+
+print(dados.items())
+# for chave, valor in dados.items():
+#     print(chave)
+#     print(valor)
+#
+# for chave, valor in receitas.items():
+#     print(chave)
+#     print(valor)
