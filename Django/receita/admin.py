6ea93981d9ef80bfb0c596523016059369f8c40c
edu-lauer <lauer.edu@gmail.com>
2022-05-20 09:37:44 -0700

    melhorado a página de admin

--- Django/receita/admin.py
+++ Django/receita/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Receita
 
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 5
+
+
+admin.site.register(Receita, ListandoReceitas)
