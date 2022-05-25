73b93071728988018c7b1bb04fe79b623de89d98
edu-lauer <lauer.edu@gmail.com>
2022-05-25 10:14:10 -0700

    validação de formulário, criação de usuário e salvar usuário no db sqlite3

--- Django/usuarios/views.py
+++ Django/usuarios/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('campo nome não pode ser em branco')
+            return redirect('cadastro')
+
+        if not email.strip():
+            print('campo email não pode ser em branco')
+            return redirect('cadastro')
+
+        if senha != senha2:
+            print('As senhas não correspondem')
+            return redirect('cadastro')
+
+        if User.objects.filter(email=email).exists():
+            print('usuário ja cadastrado')
+            return redirect('cadastro')
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('usuário cadastrado com sucesso!')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
