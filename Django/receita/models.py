62d0667a4905833da469f26131425c80663d26b9
edu-lauer <lauer.edu@gmail.com>
2022-05-25 12:22:37 -0700

    formulário para o usuário logado criar receita

--- Django/receita/models.py
+++ Django/receita/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from datetime import datetime
-from pessoas.models import Pessoa
+from django.contrib.auth.models import User
 
 
 class Receita(models.Model):
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     objects = models.Manager
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
