61663cb280c5a865a5b0c6f16f430e7ce9a49dc5
edu-lauer <lauer.edu@gmail.com>
2022-06-02 06:07:41 -0700

    novos campos e alterado visual

--- Django-validacao-formulario/passagens/forms.py
+++ Django-validacao-formulario/passagens/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.forms import Textarea
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from passagens.classe_viagem import tipos_de_classe
 
 
 class PassagemForms(forms.Form):
@@ -7,3 +10,7 @@ class PassagemForms(forms.Form):
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today())
+    classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipos_de_classe)
+    informacoes = forms.CharField(label='Informações extra', max_length=200, widget=Textarea(), required=False)
+    email = forms.EmailField(label='e-mail', max_length=150)
